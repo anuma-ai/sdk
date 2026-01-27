31d2c3ce7274f53cbc8097bf61a86ed9d0c3886d
rutwik2001 <rutwikpasumarthi.01@gmail.com>
2026-01-27 15:55:43 -0800

    Added media operations

---
+++ src/lib/db/media/index.ts
@@ -0,0 +1,57 @@
+// Models
+export { Media } from "./models";
+
+// Types
+export type {
+  MediaType,
+  MediaRole,
+  MediaDimensions,
+  MediaMetadata,
+  StoredMedia,
+  CreateMediaOptions,
+  UpdateMediaOptions,
+  MediaFilterOptions,
+  MediaOperationsContext,
+} from "./types";
+
+// Utility functions
+export {
+  generateMediaId,
+  getMediaTypeFromMime,
+  isSupportedMediaType,
+} from "./types";
+
+// Operations - CRUD
+export {
+  mediaToStored,
+  createMediaOp,
+  createMediaBatchOp,
+  getMediaByIdOp,
+  getMediaBySourceUrlOp,
+  updateMediaOp,
+  deleteMediaOp,
+  hardDeleteMediaOp,
+} from "./operations";
+
+// Operations - Library queries
+export {
+  getMediaOp,
+  getMediaByTypeOp,
+  getImagesOp,
+  getVideosOp,
+  getAudioOp,
+  getDocumentsOp,
+  getMediaByConversationOp,
+  getMediaByMessageOp,
+  getMediaByIdsOp,
+  getMediaByRoleOp,
+  getAIGeneratedMediaOp,
+  getUserUploadedMediaOp,
+  getMediaByModelOp,
+  getRecentMediaOp,
+  searchMediaOp,
+  getMediaCountOp,
+  getMediaCountsByTypeOp,
+  deleteMediaByConversationOp,
+  deleteMediaByMessageOp,
+} from "./operations";
