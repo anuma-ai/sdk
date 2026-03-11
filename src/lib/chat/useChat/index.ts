800a0c1b7a82186abae24a7ccd536d9e8f920bb4
Denis Fadeev <denis@fadeev.org>
2026-03-11 15:20:16 +0300

    fix: remove unused RunToolLoopOptions type export from useChat index

--- src/lib/chat/useChat/index.ts
+++ src/lib/chat/useChat/index.ts
@@ -3,4 +3,4 @@ export { StreamSmoother } from "./StreamSmoother";
 export * from "./types";
 export * from "./utils";
 export { runToolLoop } from "../toolLoop";
-export type { AutoExecutedToolResult, RunToolLoopOptions, RunToolLoopResult } from "../toolLoop";
+export type { AutoExecutedToolResult, RunToolLoopResult } from "../toolLoop";
